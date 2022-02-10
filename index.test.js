8cce14a8c488991642bf142402475861d68add38
sapphi-red <green@sapphi.red>
2022-02-11 01:48:13 +0900

    Migrate to uvu from jest

--- index.test.js
+++ index.test.js
@@ -2,6 +2,8 @@ const Md = require('markdown-it')
 const spoiler = require('./index');
 const testLoad = require('markdown-it-testgen').load
 const path = require('path')
+const { test } = require('uvu')
+const assert = require('uvu/assert')
 
 const md = Md().use(spoiler, false)
 const mdFrontPrior = Md().use(spoiler, true)
@@ -13,7 +15,7 @@ testLoad(path.join(__dirname, 'fixtures/default.txt'), data => {
       const actual = md.render(fixture.first.text)
       const expected = fixture.second.text
 
-      expect(actual).toBe(expected)
+      assert.is(actual, expected)
     })
 
   })
@@ -26,8 +28,10 @@ testLoad(path.join(__dirname, 'fixtures/frontPrior.txt'), data => {
       const actual = mdFrontPrior.render(fixture.first.text)
       const expected = fixture.second.text
 
-      expect(actual).toBe(expected)
+      assert.is(actual, expected)
     })
 
   })
 })
+
+test.run()
